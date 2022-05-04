b5c4fa76b9adcbfd5bd51b84a61ec1f39efb5521
Steve <15229911+stevehanstudio@users.noreply.github.com>
2022-05-03 17:34:56 -0700

    Clean up code and modify README.md

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,3 @@
-// SerialPort Upgrade Guide - from 9.x to 10.x
-// https://serialport.io/docs/guide-upgrade
-
-// realtime dat viz https://itnext.io/real-time-sensor-data-visualisation-using-reactjs-nodejs-socket-io-and-raspberry-pi-d109fd5cb19d
-
-// Arudino, NodeJS, Chart.js https://blog.priyanshrastogi.com/introduction-to-internet-of-things-with-arduino-node-js-and-chart-js-cb4885d176c4
-
 import * as http from 'http'
 import express, { Express, json, Request, Response } from 'express'
 import cors from 'cors'
@@ -42,7 +35,7 @@ const io = new Server(server, {
 	}
 })
 
-console.log('SerialPort:', SerialPort)
+// console.log('SerialPort:', SerialPort)
 // const ReadLine = SerialPort.parsers.Readline
 const port = new SerialPort({ path: 'COM7', baudRate: 9600 })
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
@@ -55,39 +48,6 @@ parser.on('data', (data: string) => {
 parser.on('error', (error: Error) => {
 	console.log(error)
 })
-
-// let interval
-
-// io.on('connection', client => {
-// 	console.log(`Connected to the client: ${client.id}`)
-	// if (interval) {
-	// 	clearInterval(interval)
-	// }
-	// interval = setInterval(() => {
-
-	// }, 1000)
-
-	// client.on('change_temp_type', data => {
-	// 	port.write(data.tempType, (err: Error | null | undefined) => {
-	// 		if (err)
-	// 			return console.log('Error on write:', err.message);
-	// 	})
-	// 	console.log('message written:', data.tempType);
-	// })
-
-	// client.on('disconnect', () => {
-	// 	console.log('Disconnected')
-	// 	clearInterval(interval)
-	// })
-
-	// client.on('subscribeToTemperature', interval => {
-	// 	console.log('Client is subscribing with interval', interval);
-	// })
-
-	// setInterval(() => {
-	// 	client.emit('getTemperature', 10)
-	// }, interval)
-// })
 
 server.listen(5000, () => {
 	console.log('Listening on port 5000')
--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -13,15 +13,8 @@ export type AppContextType = {
 export const AppContext = createContext<AppContextType>({} as AppContextType)
 // export const AppContext = createContext<AppContextType | null>(null)
 
-// export const useGlobalContext = () => {
-//   return useContext(AppContext)
-// }
-
 export const AppProvider = ({ children }: { children: ReactNode }): ReactElement => {
   const [state, dispatch] = useReducer(appReducer, initialState)
-  // const [totalCount, setTotalCount] = useState(0)
-  // const [data, setData] = useState<DataItem[]>([])
-  // const [tempType, setTempType] = useState(TempType.C)
 
   const toggleTempType = () => {
     dispatch({
@@ -30,17 +23,10 @@ export const AppProvider = ({ children }: { children: ReactNode }): ReactElement
   }
 
   const addData = (dataItem: DataItem) => {
-    // const updatedDataItem = state.data.count(dataItem)
-
     dispatch({
       type: "ADD_DATA",
       payload: dataItem
     })
-
-    // setData([...data, dataItem])
-    // // data.push(dataItem)
-    // setTotalCount(totalCount+1)
-    // console.log('Adding data', totalCount)
   }
 
   const value = {
